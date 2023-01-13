b03e10c3c9a0595850903f0dc80ca894a42e44db
Este Thy <nguyenthy18401@gmail.com>
2023-01-13 16:13:37 +0700

    add to practice

--- Challenge 15-4-22/code.js
+++ Challenge 15-4-22/code.js
@@ -11,3 +11,13 @@ function century(year) {
       return(firstHalf +=1)
     }
 }
+// Practice 1
+function century(year) {
+  let firstHalf = Math.floor(year / 100) //get first 2 digits, in case the result gives decimal numbers like 17.05 or 0.8, use Math.floor to get the integer numbers
+  let lastHalf = year % 100 // get last 2 digits
+  if(lastHalf === 0 ){
+    return(firstHalf)
+  } else {
+    return(firstHalf +=1)
+  }
+}
